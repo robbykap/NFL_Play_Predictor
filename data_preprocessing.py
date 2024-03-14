41c744394b2291b9d9297cf18854b964596790fc
Robert Greathouse <robbykap@byu.edu>
2024-03-13 19:17:11 -0600

    Cleaning features

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,7 +2,6 @@ import pandas as pd
 
 
 features_to_keep = [
-    # 'home_teaplay_idm',  # not sure what this feature is
     'posteam',
     'defteam',
     'yardline_100',
@@ -18,8 +17,6 @@ features_to_keep = [
     'ydstogo',
     'ydsnet',
     'play_type',
-    'qb_dropback',
-    'qb_scramble',
     'posteam_timeouts_remaining',
     'defteam_timeouts_remaining',
     'posteam_score',
@@ -42,8 +39,30 @@ def load_nfl_data(file_path) -> [pd.DataFrame, pd.Series]:
     # remove any plays where play_type not in ('pass', 'run')
     nfl_play_data = nfl_play_data[nfl_play_data['play_type'].isin(['pass', 'run'])]
 
+    # convert the number of seconds to hours
+    # nfl_play_data = nfl_play_data['quarter_seconds_remaining'] / 3600
+    # nfl_play_data = nfl_play_data['half_seconds_remaining'] / 3600
+    # nfl_play_data = nfl_play_data['game_seconds_remaining'] / 3600
+
+    # convert the the following feature to a dummy variables
+    # nfl_play_data = nfl_play_data['game_half'].get_dummies()
+    # nfl_play_data = nfl_play_data['postteam'].get_dummies()
+    # nfl_play_data = nfl_play_data['defteam'].get_dummies()
+
     play_outcomes: pd.Series = nfl_play_data.pop('play_type')
 
     print(len(nfl_play_data))
 
     return nfl_play_data, play_outcomes
+
+def missing_data():
+    nfl_play_data, play_outcomes = load_nfl_data('nfl-play-by-play-2009-2018.csv')
+    print(nfl_play_data.columns)
+
+
+    
+
+
+
+missing_data()
+
