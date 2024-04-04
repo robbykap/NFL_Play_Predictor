ae2cbf6e31e6005030fa51284bd07bf2329a0c0e
Zachary Burns <zacharyburns001@gmail.com>
2024-04-03 19:39:09 -0600

    bad code

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -23,27 +23,65 @@ features_to_keep = [
     'defteam_score',
     'score_differential',
     'ep',
-    'epa'
+    'epa',
+    'previous_play1',
+    'previous_play2',
+    'previous_play3'
 ]
+
+
+def set_previous_play1(row):
+    if row['drive'] != row['previous_drive1']:
+        return 'new_team'
+    else:
+        return row['previous_play1']
+
+
+def set_previous_play2(row):
+    if row['drive'] != row['previous_drive2']:
+        return 'new_team'
+    else:
+        return row['previous_play2']
+
+
+def set_previous_play3(row):
+    if row['drive'] != row['previous_drive3']:
+        return 'new_team'
+    else:
+        return row['previous_play3']
 
 
 def load_nfl_data(file_path) -> [pd.DataFrame, pd.Series]:
 
     nfl_play_data: pd.DataFrame = pd.read_csv(file_path)
 
+    # jank
+    nfl_play_data[f'previous_play1'] = nfl_play_data['play_type'].shift(1)
+    nfl_play_data[f'previous_drive1'] = nfl_play_data['drive'].shift(1)
+
+    nfl_play_data[f'previous_play2'] = nfl_play_data['play_type'].shift(2)
+    nfl_play_data[f'previous_drive2'] = nfl_play_data['drive'].shift(2)
+
+    nfl_play_data[f'previous_play3'] = nfl_play_data['play_type'].shift(3)
+    nfl_play_data[f'previous_drive3'] = nfl_play_data['drive'].shift(3)
+
+    # add column for previous play(s)
+    nfl_play_data['previous_play1'] = nfl_play_data.apply(set_previous_play1, axis=1)
+    nfl_play_data['previous_play2'] = nfl_play_data.apply(set_previous_play2, axis=1)
+    nfl_play_data['previous_play3'] = nfl_play_data.apply(set_previous_play3, axis=1)
+
     # keep only the columns listed
     nfl_play_data = nfl_play_data[features_to_keep]
 
-    nfl_play_data = nfl_play_data.dropna()
-
     # remove any plays where play_type not in ('pass', 'run')
     nfl_play_data = nfl_play_data[nfl_play_data['play_type'].isin(['pass', 'run'])]
+
+    nfl_play_data = nfl_play_data.dropna()
 
     # convert the number of seconds to hours
     nfl_play_data['quarter_seconds_remaining'] = nfl_play_data['quarter_seconds_remaining'] / 3600
     nfl_play_data['half_seconds_remaining'] = nfl_play_data['half_seconds_remaining'] / 3600
     nfl_play_data['game_seconds_remaining'] = nfl_play_data['game_seconds_remaining'] / 3600
-
 
     play_outcomes = pd.get_dummies(nfl_play_data.pop('play_type'))
 
